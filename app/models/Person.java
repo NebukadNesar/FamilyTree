f27c31e203819f25dbeee4da16ae9e0c326088cf
burhan cerit <tavatr@tavatr.(none)>
2013-04-11 23:34:24 +0300

    Uygulama oluşturuldu ve basic sayfalar yapıldı.

---
+++ app/models/Person.java
@@ -0,0 +1,186 @@
+package models;
+
+import java.sql.Date;
+import java.util.ArrayList;
+
+import javax.persistence.Entity;
+
+import play.data.validation.Required;
+import play.db.jpa.Blob;
+import play.db.jpa.Model;
+
+@Entity
+public class Person extends Model {
+
+	@Required
+	private String name;
+	@Required
+	private String surname;
+	@Required
+	private String gender;
+	@Required
+	private Date birthdate;
+	private Date deathdate;
+	private String eyecolor;
+	private int height;
+
+	@Required
+	private String mothername;
+	@Required
+	private String fathername;
+	private int childrennumber;
+	@Required
+	private String primaryschoolname;
+	@Required
+	private String secondaryschoolname;
+	@Required
+	private String universityname;
+	@Required
+	private String job;
+
+	private String generaldescription;
+	private ArrayList<Blob> pictures;
+
+	private ArrayList<Person> listOfChilds;
+
+	public Person() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getSurname() {
+		return surname;
+	}
+
+	public void setSurname(String surname) {
+		this.surname = surname;
+	}
+
+	public Date getBirthdate() {
+		return birthdate;
+	}
+
+	public void setBirthdate(Date birthdate) {
+		this.birthdate = birthdate;
+	}
+
+	public Date getDeathdate() {
+		return deathdate;
+	}
+
+	public void setDeathdate(Date deathdate) {
+		this.deathdate = deathdate;
+	}
+
+	public String getEyecolor() {
+		return eyecolor;
+	}
+
+	public void setEyecolor(String eyecolor) {
+		this.eyecolor = eyecolor;
+	}
+
+	public int getHeight() {
+		return height;
+	}
+
+	public void setHeight(int height) {
+		this.height = height;
+	}
+
+	public String getMothername() {
+		return mothername;
+	}
+
+	public void setMothername(String mothername) {
+		this.mothername = mothername;
+	}
+
+	public String getFathername() {
+		return fathername;
+	}
+
+	public void setFathername(String fathername) {
+		this.fathername = fathername;
+	}
+
+	public int getChildrennumber() {
+		return childrennumber;
+	}
+
+	public void setChildrennumber(int childrennumber) {
+		this.childrennumber = childrennumber;
+	}
+
+	public String getPrimaryschoolname() {
+		return primaryschoolname;
+	}
+
+	public void setPrimaryschoolname(String primaryschoolname) {
+		this.primaryschoolname = primaryschoolname;
+	}
+
+	public String getSecondaryschoolname() {
+		return secondaryschoolname;
+	}
+
+	public void setSecondaryschoolname(String secondaryschoolname) {
+		this.secondaryschoolname = secondaryschoolname;
+	}
+
+	public String getUniversityname() {
+		return universityname;
+	}
+
+	public void setUniversityname(String universityname) {
+		this.universityname = universityname;
+	}
+
+	public String getJob() {
+		return job;
+	}
+
+	public void setJob(String job) {
+		this.job = job;
+	}
+
+	public String getGeneraldescription() {
+		return generaldescription;
+	}
+
+	public void setGeneraldescription(String generaldescription) {
+		this.generaldescription = generaldescription;
+	}
+
+	public ArrayList<Blob> getPictures() {
+		return pictures;
+	}
+
+	public void setPictures(ArrayList<Blob> pictures) {
+		this.pictures = pictures;
+	}
+
+	public ArrayList<Person> getListOfChilds() {
+		return listOfChilds;
+	}
+
+	public void setListOfChilds(ArrayList<Person> listOfChilds) {
+		this.listOfChilds = listOfChilds;
+	}
+
+	public String getGender() {
+		return gender;
+	}
+
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+
+}
